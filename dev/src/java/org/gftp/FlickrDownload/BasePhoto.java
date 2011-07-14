95b2c33d12610c817342e568764a023b58f020ec
Brian Masney <masneyb@gmail.com>
2011-07-14 01:37:41 +0000

    updated README and copyright on some of the files
    git-svn-id: https://flickrdownload.googlecode.com/svn/trunk@50 4098e2c6-3a3f-0410-89e7-d30cb3578592

--- dev/src/java/org/gftp/FlickrDownload/BasePhoto.java
+++ dev/src/java/org/gftp/FlickrDownload/BasePhoto.java
@@ -1,3 +1,14 @@
+/*
+  FlickrDownload - Copyright(C) 2011 Brian Masney <masneyb@gmail.com>.
+  If you have any questions, comments, or suggestions about this program, please
+  feel free to email them to me. You can always find out the latest news about
+  FlickrDownload from my website at http://code.google.com/p/flickrdownload/
+
+  FlickrDownload comes with ABSOLUTELY NO WARRANTY; for details, see the COPYING
+  file. This is free software, and you are welcome to redistribute it under
+  certain conditions; for details, see the COPYING file.
+*/
+
 package org.gftp.FlickrDownload;
 
 import com.aetrion.flickr.photos.Photo;
